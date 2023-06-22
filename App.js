e30f6d248ca941cb0624c9e7071bbd043e2cab66
eocampo2021 <eocampo@doxelcontrol.com>
2023-06-21 23:39:40 -0300

    Cambio del titulo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola, esto es la entrega del desafio1 !</Text>
+      <Text>Entrega del desafio 1</Text>
       <StatusBar style="auto" />
     </View>
   );
