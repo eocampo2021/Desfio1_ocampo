b2463f99baa8cb3f6cac636749c984e1508758fe
eocampo2021 <eocampo@doxelcontrol.com>
2023-06-21 23:25:53 -0300

    Primer commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola, esto es la entrega del desafio1 !</Text>
       <StatusBar style="auto" />
     </View>
   );
